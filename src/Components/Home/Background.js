103f437997990ab265ea58f5d6993387491f2a17
fayez1-10-2004 <fayzm5575@gmail.com>
2025-06-04 19:54:33 +0300

    update

--- src/Components/Home/Background.js
+++ src/Components/Home/Background.js
@@ -1,25 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bgstyle from './Background.module.css'
-import bg2 from '../../images/bg2.jpg';
+import bg2 from '../../images/banner1.jpg';
+import bg1 from '../../images/banner2.jpg'
+import bg3 from '../../images/banner3.jpg'
+import { MdArrowForwardIos } from "react-icons/md";
+import { MdArrowBackIos } from "react-icons/md"
 const Background = (featured,setfeatured) => {
+const [cureentIndex,setcureentIndex]=useState(0)
+const [actiuveIndex,setactiuveIndex]=useState(null)
+const collect=[{
+    image:bg1,
+    descrption:'Get up to 50% off Today Only!',
+    title:'Woman Fashion',
+},
+{
+    image:bg2,
+    descrption:'50% off in all products!',
+    title:'Man Fashion',
+},
+
+{
+    image:bg3,
+    descrption:'Taking your Viewing Experience to Next Level',
+    title:'Summer Sale',
+}
+]
+const changeBackgroundnex=()=>{
+const nextIndex=(cureentIndex+1)%collect.length
+setcureentIndex(nextIndex)
+console.log(cureentIndex)
+}
+const changeBackgroundles=()=>{
+
+    const lessindex=(cureentIndex - 1 + collect.length)%collect.length
+    setcureentIndex(lessindex)
+}
+
+const toggle=(index)=>{
+if(actiuveIndex===index){
+
+setactiuveIndex(null)
+
+}else {
+
+    setactiuveIndex(index)
+}  
+
+
+
+}
     return (
         <>
-
             <section className={bgstyle.background}>
+
                 <div className={bgstyle.bgdata}>
-                    <img src={bg2} alt='bg' />
-
+                 
+                    <img src={collect[cureentIndex].image} alt='bg' />
+                
                 </div>
-
                 <div className={bgstyle.bgtext}>
-
-                    <h1>WELCOME TO OUR WEBSITE</h1>
-                    <p>WE ARE THE BEST</p>
-
+                    <h1>{collect[cureentIndex].title}</h1>
+                    <p>{collect[cureentIndex].descrption}</p>
+                    <button>SHOP NOW</button>
                 </div>
-                <div className={bgstyle.bglayer}>
+                <div className={bgstyle.button}>
+ <button onClick={changeBackgroundnex}><MdArrowBackIos /></button>
+<button onClick={changeBackgroundles}><MdArrowForwardIos /></button>    
+</div>
 
-                </div>
+<div >
+
+<div onClick={()=>toggle(1)}>
+{
+
+    actiuveIndex===1?'trend':'trend'
+}
+ {actiuveIndex === 1&& <p >هذا هو المحتوى الأول.</p>}
+</div>
+
+</div>
             </section>
 
         </>
