9df12c98a1d9190e8b924d59d438bdbff5b4eacf
fayez1-10-2004 <fayzm5575@gmail.com>
2025-06-05 07:54:37 +0300

    updatehome

--- src/Components/Home/Background.js
+++ src/Components/Home/Background.js
@@ -3,6 +3,8 @@ import bgstyle from './Background.module.css'
 import bg2 from '../../images/banner1.jpg';
 import bg1 from '../../images/banner2.jpg'
 import bg3 from '../../images/banner3.jpg'
+import mid1 from '../../images/mid-img-1.jpg'
+import mid2 from '../../images/mid_img-2.jpg'
 import { MdArrowForwardIos } from "react-icons/md";
 import { MdArrowBackIos } from "react-icons/md"
 const Background = (featured,setfeatured) => {
@@ -52,6 +54,28 @@ const changeBackgroundles=()=>{
                 <div className={bgstyle.button}>
  <button onClick={changeBackgroundnex}><MdArrowBackIos /></button>
 <button onClick={changeBackgroundles}><MdArrowForwardIos /></button>    
+</div>
+
+<div className={bgstyle.ContainerImageMid}>
+    <div className={bgstyle.mid1}>
+        <img src={mid1} alt='mid1'></img>
+        <div className={bgstyle.textImageMid1}>
+<p>Super Sale</p>
+<h1>New Collection</h1>
+<button>SHop Now</button>
+        </div>
+    </div>
+        <div className={bgstyle.mid2}>
+        <img src={mid2} alt='mid2'></img>
+
+        <div className={bgstyle.textImageMid2}>
+            <p>New Season</p>
+            <h1>Sale 40% Off</h1>
+            <button>SHop Now</button>
+        </div>
+    </div>
+
+
 </div>
 
 
